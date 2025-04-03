ad5896a5cc7ddad89eb1b06832fb10fcc6fd35c0
amidou.zabre <amidou.zabre@gmail.com>
2025-04-03 11:28:43 +0000

    ApiRoot: add sport model

--- ApiRoot/locations/admin.py
+++ ApiRoot/locations/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Continent, Country, City, Confederation, Federation
+
+
+admin.site.register(Continent)
+admin.site.register(Country)
+admin.site.register(City)
+admin.site.register(Confederation)
+admin.site.register(Federation)
 # Register your models here.
--- ApiRoot/locations/models.py
+++ ApiRoot/locations/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Continent(models.Model):
     name = models.CharField(max_length=100, unique=True)  # Nom du continent (ex. Afrique, Europe)
     code = models.CharField(max_length=2, unique=True) # Code ISO (ex. AF pour l'Afrique)
+    description = models.TextField(null=True, blank=True)  # Description
     
     def __str__(self):
         return self.name
@@ -14,6 +15,7 @@ class Country(models.Model):
     code_3 = models.CharField(max_length=3, unique=True, blank=True, null=True)  # Code ISO (ex. CIV pour Côte d’Ivoire)
     code_num = models.CharField(max_length=3, unique=True, blank=True, null=True)  # Code ISO (ex. 384 pour Côte d’Ivoire)
     continent = models.ForeignKey(Continent, on_delete=models.CASCADE, related_name='countries')  # Relation avec le continent
+    description = models.TextField(null=True, blank=True)  # Description
     
     def __str__(self):
         return self.name
@@ -24,6 +26,7 @@ class Country(models.Model):
 class City(models.Model):
     name = models.CharField(max_length=100)  # Nom de la ville
     country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='cities')  # Relation avec le pays
+    description = models.TextField(null=True, blank=True)  # Description
     
     def __str__(self):
         return f"{self.name}, {self.country.name}"
@@ -35,6 +38,8 @@ class City(models.Model):
 class Confederation(models.Model):
     name = models.CharField(max_length=100, unique=True)  # Nom de la confédération (ex. UEFA, CAF)
     continent = models.ForeignKey(Continent, on_delete=models.CASCADE, related_name='confederations')  # Relation avec le continent
+    sport = models.ForeignKey('Sport', on_delete=models.CASCADE, related_name='confederations')  # Relation avec le sport
+    description = models.TextField(null=True, blank=True)  # Description
     
     def __str__(self):
         return self.name
@@ -42,8 +47,18 @@ class Confederation(models.Model):
 class Federation(models.Model):
     name = models.CharField(max_length=100)  # Nom de la fédération (ex. Fédération Française de Football)
     country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='federations')  # Relation avec le pays
+    sport = models.ForeignKey('Sport', on_delete=models.CASCADE, related_name='federations')  # Relation avec le sport
     confederation = models.ForeignKey(Confederation, on_delete=models.SET_NULL, null=True, related_name='federations')  # Relation avec la confédération
+    description = models.TextField(null=True, blank=True)  # Description
     
     def __str__(self):
         return self.name
 
+
+class Sport(models.Model):
+    name = models.CharField(max_length=100, unique=True)  # Nom du sport (ex. Football, Basket)
+    description = models.TextField(null=True, blank=True)  # Description ou règles du sport (facultatif)
+    is_team_sport = models.BooleanField(default=True)  # Indique si c'est un sport d'équipe ou individuel
+    
+    def __str__(self):
+        return self.name
