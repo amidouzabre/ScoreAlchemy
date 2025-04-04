d4027e0f1d537d534dfee4750ae6769d1fe9301f
amidou.zabre <amidou.zabre@gmail.com>
2025-04-04 21:21:00 +0000

    Apiroot: add federation_type on Federation

--- ApiRoot/federations/models.py
+++ ApiRoot/federations/models.py
@@ -5,8 +5,14 @@ from sports.models import Sport
 
 # Create your models here.
 class Federation(models.Model):
+    TYPE_CHOICES = [
+        ('MONDIAL', 'Mondial'),
+        ('CONTINENTAL', 'Continental'),
+        ('NATIONAL', 'National'),
+    ]
     name = models.CharField(max_length=100)  # ex: UEFA
     description = models.TextField(blank=True, null=True)
+    federation_type = models.CharField(max_length=15, choices=TYPE_CHOICES, default="NATIONAL") 
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name="sub_federations")
     continent = models.ForeignKey(Continent, on_delete=models.CASCADE, null=True, blank=True, related_name="federations")
     country = models.ForeignKey(Country, on_delete=models.CASCADE, null=True, blank=True, related_name="federations")
