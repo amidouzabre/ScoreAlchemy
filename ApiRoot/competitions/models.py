f83ed9e2d7951512db5fd61713e072de73623af1
amidou.zabre <amidou.zabre@gmail.com>
2025-04-04 16:32:48 +0000

    Apiroot: Create Tournament model

--- ApiRoot/competitions/models.py
+++ ApiRoot/competitions/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from federations.models import Federation
+# Create your models here
 
-# Create your models here.
+class Tournament(models.Model):
+    TYPE_CHOICES = [
+        ('LEAGUE', 'League'),
+        ('COMPETITION', 'Competition'),
+    ]
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    logo = models.ImageField(upload_to="upaloads/tournaments/logos", blank=True, null=True)
+    tournament_type = models.CharField(max_length=15, choices=TYPE_CHOICES)  # League ou Competition
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
+    ranking = models.IntegerField(default=1)  # Classement (optionnel)
+    is_national = models.BooleanField(default=True)  # Indique si c'est un championnat national
+    federation = models.ForeignKey(Federation, on_delete=models.CASCADE, related_name="tournaments")
+
+    def __str__(self):
+        return self.name
